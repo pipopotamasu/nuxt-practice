183c2b3a66aba8356a38e5c158d65e37a33f53ab
pipopotamasu <ec0156hx@gmail.com>
2017-12-12 21:30:08 +0900

    search func

--- pages/reddits.vue
+++ pages/reddits.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <input type="text"  v-model="input" /><button @click="searchReddit">search</button>
     <ul>
       <li v-for="article in articles" :key="article.data.id">
         <a :href="article.data.url">
@@ -14,11 +15,25 @@
 <script>
 import axios from 'axios'
 export default {
+  data: function () {
+    return { articles: [], input: '' }
+  },
   transition: 'slide-right',
   async asyncData ({ query }) {
     const { data } = await axios.get('https://www.reddit.com/r/vue.json')
     return {
       articles: data.data.children
+    }
+  },
+  methods: {
+    searchReddit: function () {
+      let self = this
+      const word = this.input
+      axios.get(`https://www.reddit.com/r/${word}.json`).then((response) => {
+        self.articles = response.data.data.children
+      }).catch((e) => {
+        console.log(e)
+      })
     }
   }
 }
