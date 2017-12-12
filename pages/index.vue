ddbfa9e8c197dbbc50b6b27b8bd67e6e987427d2
pipopotamasu <ec0156hx@gmail.com>
2017-12-12 21:18:43 +0900

    add reddit api

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <h1>Home page</h1>
     <p><nuxt-link to="/about">About page</nuxt-link></p>
     <p><nuxt-link to="/users">Lists of users</nuxt-link></p>
+    <p><nuxt-link to="/reddits">Reddit</nuxt-link></p>
   </div>
 </template>
 
---
+++ pages/reddits.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <ul>
+      <li v-for="article in articles" :key="article.data.id">
+        <a :href="article.data.url">
+          <span>{{ article.data.title }}</span>
+        </a>
+      </li>
+    </ul>
+    <p><nuxt-link to="/">Back home</nuxt-link></p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  transition: 'slide-right',
+  async asyncData ({ query }) {
+    const { data } = await axios.get('https://www.reddit.com/r/vue.json')
+    return {
+      articles: data.data.children
+    }
+  }
+}
+</script>
